6521e61c6b7e3d724bd9b55c91752bd513c1e2aa
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-26 05:24:56 +0200

    Update Game.cs

--- AleaSim.Domain/Entities/Game.cs
+++ AleaSim.Domain/Entities/Game.cs
@@ -3,6 +3,7 @@ namespace AleaSim.Domain.Entities;
 public class Game {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty; // e.g., "Slot", "Roulette"
     public string Description { get; set; } = string.Empty;
     public decimal MinBet { get; set; }
     public decimal MaxBet { get; set; }
