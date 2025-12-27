4a1aac479ddf3ee553d1eed0018239afdf6092d2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-12-27 04:39:33 +0200

    Update IRtpEngine.cs

--- AleaSim.Domain/Interfaces/IRtpEngine.cs
+++ AleaSim.Domain/Interfaces/IRtpEngine.cs
@@ -3,9 +3,11 @@ using AleaSim.Domain.Entities;
 namespace AleaSim.Domain.Interfaces;
 
 public interface IRtpEngine {
-    bool IsOutcomeAllowed(Guid gameId, Guid userId, decimal potentialWinAmount, decimal betAmount, IGameRepository repo);
-    void RecordBet(Guid gameId, Guid userId, decimal amount, IGameRepository repo);
-    void RecordWin(Guid gameId, Guid userId, decimal amount, IGameRepository repo);
-    RTPStatistics GetGameStats(Guid gameId, IGameRepository repo);
-    RTPStatistics GetUserStats(Guid userId, IGameRepository repo);
+        // Atomically checks if win is allowed and records it if successful
+        bool ProcessWin(Guid gameId, Guid userId, decimal winAmount, decimal betAmount, IGameRepository repo);
+        
+        void RecordBet(Guid gameId, Guid userId, decimal amount, IGameRepository repo);
+    
+        RTPStatistics GetGameStats(Guid gameId, IGameRepository repo);
+        RTPStatistics GetUserStats(Guid userId, IGameRepository repo);
 }
