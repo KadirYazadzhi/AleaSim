7a900d05ba4c8a642da79ade2516457ce5af43ab
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-02 07:23:37 +0200

    Implement Social Lobby: Live Winners feed, Tournament banners, and Leaderboard UI

--- AleaSim.Client/obj/Debug/net8.0/AleaSim.Client.AssemblyInfo.cs
+++ AleaSim.Client/obj/Debug/net8.0/AleaSim.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleaSim.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acd17abd7b2e886eae7f90854106fb6d91188dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56bec9440d3f0289e41274ccc83372d8c7da1279")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleaSim.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleaSim.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
