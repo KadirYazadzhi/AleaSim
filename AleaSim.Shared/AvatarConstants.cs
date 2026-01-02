1b97116e34bdf0cdd51f7882f09040d9ef3a1fee
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-02 08:11:04 +0200

    Implement Avatar Gallery and Player Persona stats in Profile UI

---
+++ AleaSim.Shared/AvatarConstants.cs
@@ -0,0 +1,14 @@
+namespace AleaSim.Shared;
+
+public static class AvatarConstants {
+    public static readonly List<string> PredefinedAvatars = new() {
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Lucky",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Leper",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Gold",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Dice",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Seven",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Clover",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Admin",
+        "https://api.dicebear.com/7.x/bottts/svg?seed=Whale"
+    };
+}
--- AleaSim.Shared/obj/Debug/net8.0/AleaSim.Shared.AssemblyInfo.cs
+++ AleaSim.Shared/obj/Debug/net8.0/AleaSim.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8f511faf5ed10cf18668da849a6dd710e0ceed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112b10b9bb235ba086841e1924aa2e3efb0b7316")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
