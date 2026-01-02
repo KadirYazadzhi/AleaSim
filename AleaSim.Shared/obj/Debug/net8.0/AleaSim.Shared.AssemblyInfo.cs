e4468e458dd62a6a886624cbb3ff97f8f95fcad2
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-01-02 07:25:35 +0200

    Implement Blackjack Royale UI and game state management in Blazor

--- AleaSim.Shared/obj/Debug/net8.0/AleaSim.Shared.AssemblyInfo.cs
+++ AleaSim.Shared/obj/Debug/net8.0/AleaSim.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a900d05ba4c8a642da79ade2516457ce5af43ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d0a61d48484e4ae2b5c6ab8f5d863649902719")]
 [assembly: System.Reflection.AssemblyProductAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AleaSim.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
