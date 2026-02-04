b4b61142430974ea8c3c71e00fbde1958ed66ae1
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-02-04 20:05:06 +0200

    feat: localize audio assets to ensure offline availability

--- AleaSim.Client/wwwroot/js/audio.js
+++ AleaSim.Client/wwwroot/js/audio.js
@@ -1,10 +1,10 @@
 window.aleaAudio = {
     sounds: {},
     init: function () {
-        this.sounds['spin'] = new Audio('https://assets.mixkit.co/active_storage/sfx/2013/2013-preview.mp3');
-        this.sounds['win'] = new Audio('https://assets.mixkit.co/active_storage/sfx/1435/1435-preview.mp3');
-        this.sounds['bigwin'] = new Audio('https://assets.mixkit.co/active_storage/sfx/1431/1431-preview.mp3');
-        this.sounds['click'] = new Audio('https://assets.mixkit.co/active_storage/sfx/2568/2568-preview.mp3');
+        this.sounds['spin'] = new Audio('sounds/spin.mp3');
+        this.sounds['win'] = new Audio('sounds/win.mp3');
+        this.sounds['bigwin'] = new Audio('sounds/bigwin.mp3');
+        this.sounds['click'] = new Audio('sounds/click.mp3');
     },
     play: function (name) {
         if (this.sounds[name]) {
