30d4dd8567cccd3ab46c838f8cae041678e08b11
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2026-02-15 02:48:32 +0200

    Implement theme persistence with LocalStorage and async initialization

--- AleaSim.Client/Services/LayoutService.cs
+++ AleaSim.Client/Services/LayoutService.cs
@@ -1,22 +1,42 @@
 using System;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
 
 namespace AleaSim.Client.Services;
 
 public class LayoutService
 {
+    private readonly ILocalStorageService _localStorage;
+
+    public LayoutService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+
     public bool IsDarkMode { get; private set; } = true;
 
     public event Action? OnMajorUpdate;
 
-    public void SetDarkMode(bool value)
+    public async Task InitializeAsync()
+    {
+        if (await _localStorage.ContainKeyAsync("darkMode"))
+        {
+            IsDarkMode = await _localStorage.GetItemAsync<bool>("darkMode");
+            OnMajorUpdate?.Invoke();
+        }
+    }
+
+    public async Task SetDarkMode(bool value)
     {
         IsDarkMode = value;
+        await _localStorage.SetItemAsync("darkMode", IsDarkMode);
         OnMajorUpdate?.Invoke();
     }
 
-    public void ToggleDarkMode()
+    public async Task ToggleDarkMode()
     {
         IsDarkMode = !IsDarkMode;
+        await _localStorage.SetItemAsync("darkMode", IsDarkMode);
         OnMajorUpdate?.Invoke();
     }
 }
